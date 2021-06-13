a2cb9025aed63f3a6b0f5b85cd4afa37e4e249f0
Po Sicamois <sicamois@gmail.com>
2021-06-13 21:31:53 +0200

    Refactor code

--- src/naim-uniti-receiver.ts
+++ src/naim-uniti-receiver.ts
@@ -1,88 +1,163 @@
 import {
-  AccessoryPlugin,
-  Categories,
-  CharacteristicGetCallback,
+  API,
+  APIEvent,
+  CharacteristicEventTypes,
   CharacteristicSetCallback,
   CharacteristicValue,
+  DynamicPlatformPlugin,
   HAP,
   Logging,
-  Service,
-  CharacteristicEventTypes,
-  Characteristic,
+  PlatformAccessory,
+  PlatformAccessoryEvent,
+  PlatformConfig,
 } from 'homebridge';
-
 import axios from 'axios';
-import { HomebridgeAPI } from 'homebridge/lib/api';
 
+const PLUGIN_NAME = 'homebridge-naim-uniti-reveiver';
+const PLATFORM_NAME = 'NaimUnitiReceiver';
 const NAIM_API_PORT = 15081;
 
-export class NaimUnitiReceiver implements AccessoryPlugin {
-  public name: string;
-  public category: Categories;
+/*
+ * IMPORTANT NOTICE
+ *
+ * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module (or the "hap-nodejs" module).
+ * The above import block may seem like, that we do exactly that, but actually those imports are only used for types and interfaces
+ * and will disappear once the code is compiled to Javascript.
+ * In fact you can check that by running `npm run build` and opening the compiled Javascript file in the `dist` folder.
+ * You will notice that the file does not contain a `... = require("homebridge");` statement anywhere in the code.
+ *
+ * The contents of the above import statement MUST ONLY be used for type annotation or accessing things like CONST ENUMS,
+ * which is a special case as they get replaced by the actual value and do not remain as a reference in the compiled code.
+ * Meaning normal enums are bad, const enums can be used.
+ *
+ * You MUST NOT import anything else which remains as a reference in the code, as this will result in
+ * a `... = require("homebridge");` to be compiled into the final Javascript code.
+ * This typically leads to unexpected behavior at runtime, as in many cases it won't be able to find the module
+ * or will import another instance of homebridge causing collisions.
+ *
+ * To mitigate this the {@link API | Homebridge API} exposes the whole suite of HAP-NodeJS inside the `hap` property
+ * of the api object, which can be acquired for example in the initializer function. This reference can be stored
+ * like this for example and used to access all exported variables and classes from HAP-NodeJS.
+ */
+let hap: HAP;
+let Accessory: typeof PlatformAccessory;
 
+export = (api: API) => {
+  hap = api.hap;
+  Accessory = api.platformAccessory;
+
+  api.registerPlatform(PLATFORM_NAME, ExampleDynamicPlatform);
+};
+
+type context = {
+  ip: string;
+};
+
+class ExampleDynamicPlatform implements DynamicPlatformPlugin {
   private readonly log: Logging;
+  private readonly api: API;
+  private readonly config: PlatformConfig;
 
-  private readonly atomService: Service;
-  private readonly atomSpeakerService: Service;
-  private readonly informationService: Service;
+  private readonly accessories: PlatformAccessory[] = [];
 
-  private powerOn: boolean;
-  private mute: boolean;
-  private volume: number;
-  private currentMediaState: number;
-
-  constructor(hap: HAP, log: Logging, name: string, ip: string) {
+  constructor(log: Logging, config: PlatformConfig, api: API) {
     this.log = log;
-    this.name = name;
-    this.powerOn = false;
-    this.mute = false;
-    this.volume = 0;
-    this.currentMediaState = hap.Characteristic.CurrentMediaState.STOP;
-    this.category = hap.Categories.AUDIO_RECEIVER;
+    this.api = api;
+    this.config = config;
 
-    const baseURL = 'http://' + ip + ':' + NAIM_API_PORT;
+    // probably parse config or something here
 
-    this.atomService = new hap.Service.Television(name, 'Naim Unity');
-    this.atomService
+    log.info('Example platform finished initializing!');
+
+    /*
+     * When this event is fired, homebridge restored all cached accessories from disk and did call their respective
+     * `configureAccessory` method for all of them. Dynamic Platform plugins should only register new accessories
+     * after this event was fired, in order to ensure they weren't added to homebridge already.
+     * This event can also be used to start discovery of new accessories.
+     */
+    api.on(APIEvent.DID_FINISH_LAUNCHING, () => {
+      log.info("Example platform 'didFinishLaunching'");
+
+      // Parse config file to find new reveivers to register
+      this.config.receivers.forEach((receiver: any) => {
+        const isRegistered = this.accessories.some((accessory) => {
+          accessory.displayName === receiver.name;
+        });
+        if (!isRegistered) {
+          this.addAudioReceiverAccessory(receiver.name, receiver.ip_address);
+        }
+      });
+    });
+  }
+
+  /*
+   * This function is invoked when homebridge restores cached accessories from disk at startup.
+   * It should be used to setup event handlers for characteristics and update respective values.
+   */
+  configureAccessory(accessory: PlatformAccessory<context>): void {
+    this.log('Configuring accessory %s', accessory.displayName);
+
+    accessory.on(PlatformAccessoryEvent.IDENTIFY, () => {
+      this.log('%s identified!', accessory.displayName);
+    });
+
+    this.setServices(accessory);
+
+    this.accessories.push(accessory);
+  }
+
+  setServices = (accessory: PlatformAccessory<context>) => {
+    if (!accessory.context || !accessory.context.ip) {
+      this.log.error('No IP Address configured on %s', accessory.displayName);
+      return;
+    }
+
+    const baseURL = 'http://' + accessory.context.ip + ':' + NAIM_API_PORT;
+
+    const atomService = new hap.Service.Television(
+      accessory.displayName,
+      'Naim Unity'
+    );
+    atomService
       .getCharacteristic(hap.Characteristic.Active)
       .onGet(async () => {
         naimApiGet('/power', 'system')
-          .then( (returnedValue) => {
-            this.powerOn = returnedValue === 'on';
+          .then((returnedValue) => {
+            const isActive = returnedValue === 'on';
+            atomService.updateCharacteristic(
+              hap.Characteristic.Active,
+              isActive);
+            return isActive;
           })
-          .catch(error => {
+          .catch((error) => {
             handleError(error);
-          })
-          .finally( () => {
-            this.atomService.updateCharacteristic(hap.Characteristic.Active, this.powerOn);
+            return null;
           });
-        // return a guess (power toggled) as soon as possible. Update when the async finction returns
-        return this.powerOn;
+        return null;
       })
       .onSet(async (value) => {
         naimApiPut('/power', 'system', (value as boolean) ? 'on' : 'lona')
-          .then(_ => {
-            this.powerOn = value as boolean;
+          .then((_) => {
+            atomService.updateCharacteristic(
+              hap.Characteristic.Active,
+              value as boolean);
           })
-          .catch(error => {
+          .catch((error) => {
             handleError(error);
-          })
-          .finally( () => {
-            this.atomService.updateCharacteristic(hap.Characteristic.Active, this.powerOn);
           });
       });
 
-    this.atomService.setCharacteristic(hap.Characteristic.ActiveIdentifier, 1);
-    this.atomService.setCharacteristic(
+    atomService.setCharacteristic(hap.Characteristic.ActiveIdentifier, 1);
+    atomService.setCharacteristic(
       hap.Characteristic.ConfiguredName,
-      this.name);
+      accessory.displayName);
 
-    this.atomService
+    atomService
       .getCharacteristic(hap.Characteristic.CurrentMediaState)
       .onGet(async () => {
         let mediaState: number;
         naimApiGet('/nowplaying', 'transportState')
-          .then(returnedValue => {
+          .then((returnedValue) => {
             switch (returnedValue?.toString()) {
               case '2':
                 mediaState = hap.Characteristic.CurrentMediaState.PLAY;
@@ -94,24 +169,25 @@ export class NaimUnitiReceiver implements AccessoryPlugin {
                 mediaState = hap.Characteristic.CurrentMediaState.STOP;
                 break;
             }
-            this.currentMediaState = mediaState;
+            atomService.updateCharacteristic(
+              hap.Characteristic.CurrentMediaState,
+              mediaState);
+            return mediaState;
           })
-          .catch(error => {
+          .catch((error) => {
             handleError(error);
-          })
-          .finally( () => {
-            this.atomService.updateCharacteristic(hap.Characteristic.CurrentMediaState, this.currentMediaState);
+            return null;
           });
         // return as soon as possible, update on the resoution of the async function
-        return this.currentMediaState;
+        return null;
       });
 
-    this.atomService
+    atomService
       .getCharacteristic(hap.Characteristic.TargetMediaState)
       .onGet(async () => {
         let mediaState: number;
         naimApiGet('/nowplaying', 'transportState')
-          .then(returnedValue => {
+          .then((returnedValue) => {
             switch (returnedValue?.toString()) {
               case '2':
                 mediaState = hap.Characteristic.CurrentMediaState.PLAY;
@@ -123,98 +199,88 @@ export class NaimUnitiReceiver implements AccessoryPlugin {
                 mediaState = hap.Characteristic.CurrentMediaState.STOP;
                 break;
             }
-            this.currentMediaState = mediaState;
+            atomService.updateCharacteristic(
+              hap.Characteristic.CurrentMediaState,
+              mediaState);
+            return mediaState;
           })
-          .catch(error => {
+          .catch((error) => {
             handleError(error);
-          })
-          .finally( () => {
-            this.atomService.updateCharacteristic(hap.Characteristic.CurrentMediaState, this.currentMediaState);
+            return null;
           });
-        return this.currentMediaState;
+        // return as soon as possible, update on the resoution of the async function
+        return null;
       })
       .onSet(async () => {
-        naimApiPut('/nowplaying', 'cmd', 'playpause', true)
-          .then(_ => {
-            if (
-              this.currentMediaState === hap.Characteristic.CurrentMediaState.PAUSE
-            ) {
-              this.currentMediaState = hap.Characteristic.CurrentMediaState.PLAY;
-            } else {
-              this.currentMediaState = hap.Characteristic.CurrentMediaState.PAUSE;
-            }
-          })
-          .catch(error => {
-            handleError(error);
-          })
-          .finally( () => {
-            this.atomService.updateCharacteristic(hap.Characteristic.CurrentMediaState, this.currentMediaState);
-          });
+        naimApiPut('/nowplaying', 'cmd', 'playpause', true).catch((error) => {
+          handleError(error);
+        });
       });
 
-    this.atomSpeakerService = new hap.Service.TelevisionSpeaker(this.name + 'Speaker');
+    const atomSpeakerService = new hap.Service.TelevisionSpeaker(
+      accessory.displayName + 'Service');
 
-    this.atomSpeakerService
+    atomSpeakerService
       .getCharacteristic(hap.Characteristic.Mute)
       .onGet(async () => {
         naimApiGet('/levels/room', 'mute')
-          .then(returnedValue => {
-            this.mute = returnedValue === '1';
+          .then((returnedValue) => {
+            const isMuted = returnedValue === '1';
+            atomSpeakerService.updateCharacteristic(
+              hap.Characteristic.Mute,
+              isMuted);
+            return isMuted;
           })
-          .catch(error => {
+          .catch((error) => {
             handleError(error);
-          })
-          .finally( () => {
-            this.atomSpeakerService.updateCharacteristic(hap.Characteristic.Mute, this.mute);
+            return null;
           });
-        return this.mute;
+        return null;
       })
       .onSet(async (value) => {
-        naimApiPut('/levels/room', 'mute', value as string)
-          .then(_ => {
-            this.mute = value === '1';
-          })
-          .catch(error => {
-            handleError(error);
-          })
-          .finally( () => {
-            this.atomSpeakerService.updateCharacteristic(hap.Characteristic.Mute, this.mute);
-          });
+        naimApiPut('/levels/room', 'mute', value as string).catch((error) => {
+          handleError(error);
+        });
       });
 
-    this.atomSpeakerService
+    atomSpeakerService
       .getCharacteristic(hap.Characteristic.Volume)
       .onGet(async () => {
         naimApiGet('/levels/room', 'volume')
-          .then(returnedValue => {
+          .then((returnedValue) => {
             if (returnedValue) {
-              this.volume = parseInt(returnedValue);
+              const volume = parseInt(returnedValue);
+              atomSpeakerService.updateCharacteristic(
+                hap.Characteristic.Volume,
+                volume
+              );
+              return volume;
             }
           })
-          .catch(error => {
+          .catch((error) => {
             handleError(error);
-          })
-          .finally( () => {
-            this.atomSpeakerService.updateCharacteristic(hap.Characteristic.Volume, this.volume);
+            return null;
           });
-        return this.volume;
+        return null;
       })
       .onSet(async (value) => {
-        naimApiPut('/levels/room', 'volume', value as string)
-          .then( () => {
-            this.volume = value as number;
-          })
-          .catch(error => {
-            handleError(error);
-          })
-          .finally( () => {
-            this.atomSpeakerService.updateCharacteristic(hap.Characteristic.Volume, this.volume);
-          });
+        naimApiPut('/levels/room', 'volume', value as string).catch((error) => {
+          handleError(error);
+        });
       });
 
+    const informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, 'Naim')
+      .setCharacteristic(hap.Characteristic.Model, 'Uniti Atom');
+
+    atomService.addLinkedService(atomSpeakerService);
+    accessory.addService(atomService);
+    accessory.addService(informationService);
+
+    // Utility functions
     const naimApiGet = async (path: string, key: string) => {
       const apiURL = baseURL + path;
-      log.debug('naimApiCall - GET : ' + key + '@' + apiURL);
+      this.log.debug('naimApiCall - GET : ' + key + '@' + apiURL);
       try {
         const response = await axios.get(apiURL);
         return response.data[key] as string;
@@ -229,58 +295,72 @@ export class NaimUnitiReceiver implements AccessoryPlugin {
       valueToSet: string,
       forceGet = false) => {
       const apiURL = baseURL + path + '?' + key + '=' + valueToSet;
-      log.debug('naimApiCall - PUT ' + (forceGet ? '(forced)' : '') + ' : ' + valueToSet + ' into ' + key + '@' + apiURL);
+      this.log.debug(
+        'naimApiCall - PUT ' +
+          (forceGet ? '(forced)' : '') +
+          ' : ' +
+          valueToSet +
+          ' into ' +
+          key +
+          '@' +
+          apiURL
+      );
       if (!forceGet) {
-        axios.put(apiURL)
-          .catch(error => {
-            handleError(error, apiURL);
-          });
+        axios.put(apiURL).catch((error) => {
+          handleError(error, apiURL);
+        });
       } else {
-        axios.get(apiURL)
-          .catch(error => {
-            handleError(error, apiURL);
-          });
+        axios.get(apiURL).catch((error) => {
+          handleError(error, apiURL);
+        });
       }
     };
 
-    function handleError(error: Error, url = 'N/A') {
+    const handleError = (error: Error, url = 'N/A') => {
       if (axios.isAxiosError(error)) {
         if (error.response) {
           // client received an error response (5xx, 4xx)
-          log.error('Naim receiver emited a bad response (On : %s - Details : %s)', url, error.message);
+          this.log.error(
+            'Naim receiver emited a bad response (On : %s - Details : %s)',
+            url,
+            error.message);
         } else if (error.request) {
           // client never received a response, or request never left
-          log.error('Naim receiver did not respond. Check the IP Address in your configuration of the plugin. (On : %s - Details : %s)', url, error.message);
+          this.log.error(
+            'Naim receiver did not respond. Check the IP Address in your configuration of the plugin. (On : %s - Details : %s)',
+            url,
+            error.message);
         } else {
           // Not a network error
-          log.error('Other request error (On : %s - Details : %s)', url, error.message);
+          this.log.error(
+            'Other request error (On : %s - Details : %s)',
+            url,
+            error.message);
         }
       } else {
         // Not an error from the request
-        log.error('Problem (On : %s - Details : %s)', url, error.message);
+        this.log.error('Problem (On : %s - Details : %s)', url, error.message);
       }
-    }
+    };
+  };
 
-    this.informationService = new hap.Service.AccessoryInformation()
-      .setCharacteristic(hap.Characteristic.Manufacturer, 'Naim')
-      .setCharacteristic(hap.Characteristic.Model, 'Uniti Atom');
+  addAudioReceiverAccessory = (name: string, ip: string) => {
+    this.log.info('Adding new accessory with name %s', name);
 
-    log.info('Naim Uniti Receiver %s created!', name);
-  }
+    // uuid must be generated from a unique but not changing data source, name should not be used in the most cases. But works in this specific example.
+    const uuid = hap.uuid.generate(name);
+    const accessory = new Accessory<context>(
+      name,
+      uuid,
+      hap.Categories.AUDIO_RECEIVER
+    );
 
-  /*
-   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
-   * Typical this only ever happens at the pairing process.
-   */
-  identify(): void {
-    this.log('Identify!');
-  }
+    accessory.context = { ip };
 
-  /*
-   * This method is called directly after creation of this instance.
-   * It should return all services which should be added to the accessory.
-   */
-  getServices(): Service[] {
-    return [this.informationService, this.atomService, this.atomSpeakerService];
-  }
+    this.setServices(accessory); // abusing the configureAccessory here
+
+    this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [
+      accessory,
+    ]);
+  };
 }
