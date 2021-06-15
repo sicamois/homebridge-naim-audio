88a8d1a71a4aead8a4ac7a76540ce26eae84f39d
Po Sicamois <sicamois@gmail.com>
2021-06-15 22:15:35 +0200

    Refactor

---
+++ src/naimAudioAccessory.ts
@@ -0,0 +1,274 @@
+import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import axios from 'axios';
+
+import { NaimAudioPlatform, receiver } from './naimAudioPlatform';
+
+const NAIM_API_PORT = 15081;
+
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+export class NaimAudioAccessory {
+  private tvService: Service;
+  private smartSpeakerService: Service;
+  private baseURL: string;
+
+  /**
+   * These are just used to create a working example
+   * You should implement your own code to track the state of your accessory
+   */
+  private receiverStates = {
+    powerOn: false,
+    currentMediaState: this.platform.Characteristic.CurrentMediaState.STOP,
+    mute: false,
+    volume: 0,
+  };
+
+  constructor(
+    private readonly platform: NaimAudioPlatform,
+    private readonly accessory: PlatformAccessory,
+  ) {
+
+    const receiver: receiver = accessory.context.receiver;
+    this.baseURL = 'http://' + receiver.ip_address + ':' + NAIM_API_PORT;
+
+    // set accessory information
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, receiver.manufacturer || 'Naim Audio')
+      .setCharacteristic(this.platform.Characteristic.Model, receiver.modelName || 'Default-Model')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, receiver.serialNumber || 'Default-Serial');
+
+    // get the Television service if it exists, otherwise create a new Television service
+    // you can create multiple services for each accessory
+    this.tvService =
+      this.accessory.getService(this.platform.Service.Television) ||
+      this.accessory.addService(this.platform.Service.Television);
+
+    // set the service name, this is what is displayed as the default name on the Home app
+    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+    this.tvService.setCharacteristic(this.platform.Characteristic.Name, accessory.context.receiver.name);
+    this.tvService.setCharacteristic(this.platform.Characteristic.ConfiguredName, accessory.context.receiver.name);
+
+    // each service must implement at-minimum the "required characteristics" for the given service type
+    // see https://developers.homebridge.io/#/service/Lightbulb
+
+    // register handlers for the On/Off Characteristic
+    this.tvService.getCharacteristic(this.platform.Characteristic.Active)
+      .onSet(this.setActive.bind(this))
+      .onGet(this.getActive.bind(this));
+
+    this.smartSpeakerService =
+      this.accessory.getService(this.platform.Service.SmartSpeaker) ||
+      this.accessory.addService(this.platform.Service.SmartSpeaker);
+
+    // set the service name, this is what is displayed as the default name on the Home app
+    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+    this.smartSpeakerService.setCharacteristic(this.platform.Characteristic.Name, accessory.context.receiver.name);
+    this.smartSpeakerService.setCharacteristic(this.platform.Characteristic.ConfiguredName, accessory.context.receiver.name);
+
+    // register handlers for the On/Off Characteristic
+    this.smartSpeakerService.getCharacteristic(this.platform.Characteristic.CurrentMediaState)
+      .onGet(this.getTargetMediaState.bind(this));
+
+    this.smartSpeakerService.getCharacteristic(this.platform.Characteristic.TargetMediaState)
+      .onSet(this.setTargetMediaState.bind(this))
+      .onGet(this.getTargetMediaState.bind(this));
+
+    this.smartSpeakerService.getCharacteristic(this.platform.Characteristic.Volume)
+      .onSet(this.setVolume.bind(this))
+      .onGet(this.getVolume.bind(this));
+
+    this.smartSpeakerService.getCharacteristic(this.platform.Characteristic.Mute)
+      .onSet(this.setMute.bind(this))
+      .onGet(this.getMute.bind(this));
+  }
+
+  private setActive = async (value: CharacteristicValue) => {
+    const isActive = value as boolean;
+    this.receiverStates.powerOn = isActive;
+    this.naimApiPut('/power', 'system', isActive ? 'on' : 'lona').catch(
+      (error) => {
+        this.handleError(error);
+        this.receiverStates.powerOn = false;
+      });
+  };
+
+  private getActive = async (): Promise<CharacteristicValue> => {
+    let isActive = this.receiverStates.powerOn;
+    this.naimApiGet('/power', 'system')
+      .then((returnedValue) => {
+        isActive = returnedValue === 'on';
+        this.receiverStates.powerOn = isActive;
+        this.tvService.updateCharacteristic(this.platform.Characteristic.Active, isActive);
+      })
+      .catch((error) => {
+        this.handleError(error);
+        this.receiverStates.powerOn = false;
+        this.tvService.updateCharacteristic(this.platform.Characteristic.Active, false);
+      });
+    return isActive;
+  };
+
+  private setMute = async (value: CharacteristicValue) => {
+    const isMuted = value as boolean;
+    this.receiverStates.mute = isMuted;
+    this.naimApiPut('/levels/room', 'mute', value as string)
+      .catch(
+        (error) => {
+          this.handleError(error);
+          this.receiverStates.mute = false;
+        });
+  };
+
+  private getMute = async () => {
+    let isMuted = this.receiverStates.mute;
+    this.naimApiGet('/levels/room', 'mute')
+      .then((returnedValue) => {
+        isMuted = returnedValue === '1';
+        this.receiverStates.mute = isMuted;
+        this.smartSpeakerService.updateCharacteristic(this.platform.Characteristic.Mute, isMuted);
+      })
+      .catch((error) => {
+        this.handleError(error);
+        this.receiverStates.mute = false;
+        this.smartSpeakerService.updateCharacteristic(this.platform.Characteristic.Mute, false);
+      });
+    return isMuted;
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  private setTargetMediaState = async (_: CharacteristicValue) => {
+    this.receiverStates.currentMediaState === 0 ? 1 : 0;
+    this.naimApiPut('/nowplaying', 'cmd', 'playpause', true)
+      .catch((error) => {
+        this.handleError(error);
+        this.receiverStates.currentMediaState === 0 ? 1 : 0;
+      });
+  };
+
+  private getTargetMediaState = async () => {
+    let mediaState = this.receiverStates.currentMediaState;
+    this.naimApiGet('/nowplaying', 'transportState')
+      .then((returnedValue) => {
+        switch (returnedValue?.toString()) {
+          case '2':
+            mediaState = this.platform.Characteristic.CurrentMediaState.PLAY;
+            break;
+          case '3':
+            mediaState = this.platform.Characteristic.CurrentMediaState.PAUSE;
+            break;
+          default:
+            mediaState = this.platform.Characteristic.CurrentMediaState.STOP;
+            break;
+        }
+        this.receiverStates.currentMediaState = mediaState;
+        this.smartSpeakerService.updateCharacteristic(
+          this.platform.Characteristic.CurrentMediaState,
+          mediaState,
+        );
+      })
+      .catch((error) => {
+        this.handleError(error);
+        this.smartSpeakerService.updateCharacteristic(
+          this.platform.Characteristic.CurrentMediaState,
+          mediaState,
+        );
+      });
+    // return as soon as possible, update on the resoution of the async function
+    return mediaState;
+  };
+
+  private setVolume = async (value: CharacteristicValue) => {
+    const initVolume = this.receiverStates.volume;
+    const volume = +value;
+    this.receiverStates.volume = volume;
+    this.naimApiPut('/levels/room', 'volume', volume.toString())
+      .catch(
+        (error) => {
+          this.handleError(error);
+          this.receiverStates.volume = initVolume;
+        });
+  };
+
+  private getVolume = async () => {
+    let volume = this.receiverStates.volume;
+    this.naimApiGet('/levels/room', 'volume')
+      .then((returnedValue) => {
+        returnedValue = returnedValue|| '';
+        volume = +returnedValue;
+        this.receiverStates.volume = volume;
+        this.smartSpeakerService.updateCharacteristic(this.platform.Characteristic.Mute, volume);
+      })
+      .catch((error) => {
+        this.handleError(error);
+        this.smartSpeakerService.updateCharacteristic(this.platform.Characteristic.Mute, volume);
+      });
+    return volume;
+  };
+
+
+  // Utility functions
+  private naimApiGet = async (path: string, key: string) => {
+    const apiURL = this.baseURL + path;
+    this.platform.log.debug('naimApiCall - GET : ' + key + '@' + apiURL);
+    try {
+      const response = await axios.get(apiURL);
+      return response.data[key] as string;
+    } catch (error) {
+      this.handleError(error, apiURL);
+    }
+  };
+
+  private naimApiPut = async (
+    path: string,
+    key: string,
+    valueToSet: string,
+    forceGet = false,
+  ) => {
+    const apiURL = this.baseURL + path + '?' + key + '=' + valueToSet;
+    this.platform.log.debug(
+      'naimApiCall - PUT ' +
+        (forceGet ? '(forced)' : '') + ' : ' + valueToSet + ' into ' + key + '@' + apiURL);
+    if (!forceGet) {
+      axios.put(apiURL).catch((error) => {
+        this.handleError(error, apiURL);
+      });
+    } else {
+      axios.get(apiURL).catch((error) => {
+        this.handleError(error, apiURL);
+      });
+    }
+  };
+
+  private handleError = (error: Error, url = 'N/A') => {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        // client received an error response (5xx, 4xx)
+        this.platform.log.error(
+          'Naim receiver emited a bad response (On : %s - Details : %s)',
+          url,
+          error.message,
+        );
+      } else if (error.request) {
+        // client never received a response, or request never left
+        this.platform.log.error(
+          'Naim receiver did not respond. Check the IP Address in your configuration of the plugin. (On : %s - Details : %s)',
+          url,
+          error.message,
+        );
+      } else {
+        // Not a network error
+        this.platform.log.error(
+          'Other request error (On : %s - Details : %s)',
+          url,
+          error.message,
+        );
+      }
+    } else {
+      // Not an error from the request
+      this.platform.log.error('Problem (On : %s - Details : %s)', url, error.message);
+    }
+  };
+}
---
+++ src/naimAudioPlatform.ts
@@ -0,0 +1,225 @@
+import {
+  API,
+  DynamicPlatformPlugin,
+  Logger,
+  PlatformAccessory,
+  PlatformConfig,
+  Service,
+  Characteristic,
+} from 'homebridge';
+import axios from 'axios';
+import { Client, SsdpHeaders } from 'node-ssdp';
+import { Parser } from 'xml2js';
+import { RemoteInfo } from 'dgram';
+
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
+import { NaimAudioAccessory } from './naimAudioAccessory';
+
+export type receiver = {
+  name: string;
+  ip_address: string;
+  manufacturer?: string;
+  manufacturerURL?: string;
+  modelName?: string;
+  modelNumber?: string;
+  serialNumber?: string;
+  uuid?: string;
+};
+
+/**
+ * HomebridgePlatform
+ * This class is the main constructor for your plugin, this is where you should
+ * parse the user config and discover/register accessories with Homebridge.
+ */
+export class NaimAudioPlatform implements DynamicPlatformPlugin {
+  public readonly Service: typeof Service = this.api.hap.Service;
+  public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
+
+  // this is used to track restored cached accessories
+  public readonly accessories: PlatformAccessory[] = [];
+  public readonly receivers: receiver[] = [];
+
+  constructor(
+    public readonly log: Logger,
+    public readonly config: PlatformConfig,
+    public readonly api: API,
+  ) {
+    this.log.debug('Finished initializing platform:', this.config.name);
+
+    // When this event is fired it means Homebridge has restored all cached accessories from disk.
+    // Dynamic Platform plugins should only register new accessories after this event was fired,
+    // in order to ensure they weren't added to homebridge already. This event can also be used
+    // to start discovery of new accessories.
+    this.api.on('didFinishLaunching', () => {
+      log.debug('Executed didFinishLaunching callback');
+      // run the method to discover / register your devices as accessories
+      this.discoverDevices('ssdp:all', 10000);
+    });
+  }
+
+  /**
+   * This function is invoked when homebridge restores cached accessories from disk at startup.
+   * It should be used to setup event handlers for characteristics and update respective values.
+   */
+  configureAccessory(accessory: PlatformAccessory) {
+    this.log.info('Loading accessory from cache:', accessory.displayName);
+
+    // add the restored accessory to the accessories cache so we can track if it has already been registered
+    this.accessories.push(accessory);
+  }
+
+  /**
+   * This is an example method showing how to register discovered accessories.
+   * Accessories must only be registered once, previously created accessories
+   * must not be registered again to prevent "duplicate UUID" errors.
+   */
+
+  private readonly discoverDevices = (
+    uPnpType: string,
+    durationInMs: number,
+  ) => {
+    // Find Naim receiver via ssdp
+    const ssdp = new Client();
+    ssdp.on(
+      'response',
+      (headers: SsdpHeaders, statusCode: number, remoteInfos: RemoteInfo) => {
+        this.extractNaimReceiverFrom(
+          headers,
+          remoteInfos,
+          this.processReceiver,
+        );
+      });
+
+    this.log.info('Start discovering Naim Audio devices with uPnP');
+    try {
+      ssdp.search(uPnpType);
+
+      // Force ssdp discovery to stop after 10 secondes
+      setTimeout(() => {
+        ssdp.stop();
+        if (this.receivers.length === 0) {
+          this.log.warn('No Naim Audio device found on your network!');
+        }
+      }, durationInMs);
+    } catch (error) {
+      this.log.error('An error occured during discovering : %s', error.message);
+    }
+  };
+
+  private readonly extractNaimReceiverFrom = async (
+    headers: SsdpHeaders,
+    remoteInfos: RemoteInfo,
+    andProcessTheReceiver: (receiver: receiver) => void,
+  ) => {
+    const xmlParser = new Parser();
+
+    const response = await axios({
+      responseType: 'text',
+      url: headers.LOCATION,
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    xmlParser.parseString(response.data, (error: any, result: any) => {
+      if (error === null) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const device: any = result.root.device[0];
+        this.log.debug('New device found !\n   name: %s\n   manufacturer: %s\n   model: %s\n   IP Address: %s\n   uPnp type: %s',
+          device.friendlyName[0],
+          device.manufacturer[0],
+          device.modelName[0],
+          remoteInfos.address,
+          device.deviceType[0],
+        );
+        if (device) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const manufacturer: string = device.manufacturer[0];
+          if (manufacturer && manufacturer.includes('Naim')) {
+            const receiver = {
+              name: device.friendlyName[0],
+              ip_address: remoteInfos.address,
+              manufacturer: device.manufacturer[0],
+              manufacturerURL: device.manufacturerURL[0],
+              modelName: device.modelName[0],
+              modelNumber: device.modelNumber[0],
+              serialNumber: device.serialNumber[0],
+              uuid: device.UDN[0],
+            };
+            if (this.receivers.find(existingReceiver => existingReceiver.name === receiver.name)) {
+              this.log.info(
+                '%s discovered ! Already configured -> skipping',
+                receiver.name,
+              );
+              return;
+            }
+
+            this.log.info(
+              '%s discovered ! It is a %s %s',
+              receiver.name,
+              receiver.manufacturer,
+              receiver.modelName,
+            );
+            this.receivers.push(receiver);
+            andProcessTheReceiver(receiver);
+          }
+        } else {
+          this.log.error('Unable to parse response from SSDP : %s', error);
+        }
+      }
+    });
+  };
+
+  private readonly processReceiver = (receiver: receiver) => {
+    const uuid = this.api.hap.uuid.generate(receiver.name);
+
+    // see if an accessory with the same uuid has already been registered and restored from
+    // the cached devices we stored in the `configureAccessory` method above
+    const existingAccessory = this.accessories.find(
+      (accessory) => accessory.UUID === uuid,
+    );
+
+    if (existingAccessory) {
+      // the accessory already exists
+      this.log.info(
+        'Restoring existing accessory from cache:',
+        existingAccessory.displayName,
+      );
+
+      // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
+      // existingAccessory.context.device = device;
+      // this.api.updatePlatformAccessories([existingAccessory]);
+
+      // create the accessory handler for the restored accessory
+      // this is imported from `platformAccessory.ts`
+      new NaimAudioAccessory(this, existingAccessory);
+
+      // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
+      // remove platform accessories when no longer present
+      // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+      // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+    } else {
+      // the accessory does not yet exist, so we need to create it
+      this.log.info('Adding new accessory:', receiver.name);
+
+      // create a new accessory
+      const accessory = new this.api.platformAccessory(
+        receiver.name,
+        uuid,
+        this.api.hap.Categories.AUDIO_RECEIVER,
+      );
+
+      // store a copy of the device object in the `accessory.context`
+      // the `context` property can be used to store any data about the accessory you may need
+      accessory.context.receiver = receiver;
+
+      // create the accessory handler for the newly create accessory
+      // this is imported from `platformAccessory.ts`
+      new NaimAudioAccessory(this, accessory);
+
+      // link the accessory to your platform
+      this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [
+        accessory,
+      ]);
+    }
+  };
+
+}
