8f0bf9fe2abf81c3d3d9177a3186b56092afb67d
Po Sicamois <sicamois@gmail.com>
2021-06-16 18:36:25 +0200

    fix path

--- src/naimAudioAccessory.ts
+++ src/naimAudioAccessory.ts
@@ -373,7 +373,7 @@ export class NaimAudioAccessory {
 
   // Utility functions
   private naimApiGet = async (path: string, key: string) => {
-    const apiURL = this.baseURL + path.startsWith('/') ? '' : '/' + path;
+    const apiURL = this.baseURL + (path.startsWith('/') ? '' : '/') + path;
     this.platform.log.debug('naimApiCall - GET : ' + key + '@' + apiURL);
     try {
       const response = await axios.get(apiURL);
@@ -389,7 +389,7 @@ export class NaimAudioAccessory {
     valueToSet: string,
     forceGet = false,
   ) => {
-    const apiURL = this.baseURL + path.startsWith('/') ? '' : '/' + path + '?' + key + '=' + valueToSet;
+    const apiURL = this.baseURL + (path.startsWith('/') ? '' : '/') + path + '?' + key + '=' + valueToSet;
     this.platform.log.debug(
       'naimApiCall - PUT ' +
         (forceGet ? '(forced)' : '') + ' : ' + valueToSet + ' into ' + key + '@' + apiURL);
